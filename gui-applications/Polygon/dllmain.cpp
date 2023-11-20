cb5f32cdf6139ab71524de4af96d7b1efe71aabf
Pralad Prasad <pralad.prasad@gmail.com>
2023-11-20 15:45:15 +0100

    new Polygon project from ATL tutorial

---
+++ gui-applications/Polygon/dllmain.cpp
@@ -0,0 +1,16 @@
+// dllmain.cpp : Implementation of DllMain.
+
+#include "pch.h"
+#include "framework.h"
+#include "resource.h"
+#include "Polygon_i.h"
+#include "dllmain.h"
+
+CPolygonModule _AtlModule;
+
+// DLL Entry Point
+extern "C" BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID lpReserved)
+{
+	hInstance;
+	return _AtlModule.DllMain(dwReason, lpReserved);
+}
